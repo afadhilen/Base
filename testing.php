23af4a50dc067ef134cf0634ed3a5dc3b88cd52d
Ahmad Fadhil Naswir <afadhilen26@ymail.com>
2017-12-16 13:23:06 +0700

    Evaluasi Jongjet

---
+++ testing.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>Spermatophyta</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+
+    <!-- CSS Files -->
+    <link href="themes/assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="themes/assets/css/material-kit.css" rel="stylesheet" />
+
+    <!-- CSS Files -->
+    <link href="script/custom.css" rel="stylesheet" />
+</head>
+<body>
+	<?php
+		echo "<script> swal('Good job!!', 'You clicked the button!', 'success')</script>";
+	?>
+    <button onclick="swal('Good job!','You clicked the button!','success');">Test</button>
+</body>
+<script src="themes/assets/js/jquery.min.js" type="text/javascript"></script>
+<script src="themes/assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="themes/assets/js/material.min.js"></script>
+
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="themes/assets/js/nouislider.min.js" type="text/javascript"></script>
+
+<!--  Plugin for the Datepicker, full documentation here: http://www.eyecon.ro/bootstrap-datepicker/ -->
+<script src="themes/assets/js/bootstrap-datepicker.js" type="text/javascript"></script>
+
+<!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc -->
+<script src="themes/assets/js/material-kit.js" type="text/javascript"></script>
+
+<!-- Custom Script -->
+<script src="script/custom.js" type="text/javascript"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
+<script src="https://unpkg.com/sweetalert2@7.1.0/dist/sweetalert2.all.js"></script>
+</html>
